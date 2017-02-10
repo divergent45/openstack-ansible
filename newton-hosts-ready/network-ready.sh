4001e8484e91411621b063e6d52424dbe5c65720
BhujayKumarBhatta <bhujay.bhatta@gmail.com>
2017-02-09 18:14:33 -0800

    vlan-raw-device was wrong

--- newton-hosts-ready/network-ready.sh
+++ newton-hosts-ready/network-ready.sh
@@ -4,6 +4,9 @@ scp interfaces.controller2  root@controller2:/etc/network/interfaces
 ssh root@controller3 'cp /etc/network/interfaces /etc/network/interface.org'
 scp interfaces.controller3  root@controller3:/etc/network/interfaces
 
+ssh root@controller4 'cp /etc/network/interfaces /etc/network/interface.org'
+scp interfaces.controller4  root@controller4:/etc/network/interfaces
+
 ssh root@compute3 'cp /etc/network/interfaces /etc/network/interface.org'
 scp interfaces.compute3 root@compute3:/etc/network/interfaces
 
@@ -12,3 +15,4 @@ scp interfaces.compute4 root@compute4:/etc/network/interfaces
 
 ssh root@rsyslog 'cp /etc/network/interfaces /etc/network/interface.org'
 scp interfaces.rsyslog root@rsyslog:/etc/network/interfaces
+
