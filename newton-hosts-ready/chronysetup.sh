2c567a549e692533f6e33fb785bd54ca41264e1a
BhujayKumarBhatta <bhujay.bhatta@gmail.com>
2017-02-09 19:15:46 -0800

    chrony

--- newton-hosts-ready/chronysetup.sh
+++ newton-hosts-ready/chronysetup.sh
@@ -1,10 +1,12 @@
 scp chrony.conf.clients  root@controller2:/etc/chrony/chrony.conf
 scp chrony.conf.clients  root@controller3:/etc/chrony/chrony.conf
+scp chrony.conf.clients  root@controller4:/etc/chrony/chrony.conf
 scp chrony.conf.clients  root@compute3:/etc/chrony/chrony.conf
 scp chrony.conf.clients  root@compute4:/etc/chrony/chrony.conf
 scp chrony.conf.clients  root@rsyslog:/etc/chrony/chrony.conf
 ssh root@controller2 'service chrony restart'
 ssh root@controller3 'service chrony restart'
+ssh root@controller4 'service chrony restart'
 ssh root@computer3 'service chrony restart'
 ssh root@computer4 'service chrony restart'
 ssh root@rsyslog 'service chrony restart'
