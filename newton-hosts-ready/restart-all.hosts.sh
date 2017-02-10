c21315766dde22d5b47022734083fd32fac8b170
BhujayKumarBhatta <bhujay.bhatta@gmail.com>
2017-02-10 06:08:06 -0800

    working net with two NIC - mgmt+str   n vlan+vxlan

--- newton-hosts-ready/restart-all.hosts.sh
+++ newton-hosts-ready/restart-all.hosts.sh
@@ -1,6 +1,6 @@
 ssh root@controller2 reboot
 ssh root@controller3 reboot
-ssh root@controller4 reboo
+ssh root@controller4 reboot
 ssh root@compute3 reboot
 ssh root@compute4 reboot
 ssh root@rsyslog  reboot
